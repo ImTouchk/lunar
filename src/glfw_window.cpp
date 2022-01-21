ef86646acef5e2963469274f6d9ba3d1bdfacc8f
ImTouchk <imtouchk@gmail.com>
2022-01-21 19:14:51 +0200

    Implemented windowing wrapper that uses GLFW

--- src/glfw_window.cpp
+++ src/glfw_window.cpp
@@ -1,13 +1,15 @@
-#include "window.hpp"
+#include "render/window.hpp"
+#include "utils/debug.hpp"
 
 #include <vector>
+#include <cassert>
 #include <stdexcept>
 #include <functional>
 #include <GLFW/glfw3.h>
 
-struct GLFWLifeguard
+struct GlfwLifeguard
 {
-    GLFWLifeguard() noexcept
+    GlfwLifeguard() noexcept
     {
         int result;
         result = glfwInit();
@@ -15,15 +17,174 @@ struct GLFWLifeguard
             std::exit(1);
     }
 
-    ~GLFWLifeguard() noexcept
+    ~GlfwLifeguard() noexcept
     {
         glfwTerminate();
     }
 };
 
-GLFWLifeguard GLFW_LIFEGUARD = {};
+GlfwLifeguard GLFW_LIFEGUARD = {};
+
+void GLFW_KEY_CALLBACK(GLFWwindow* handle, int key, int scancode, int action, int mods)
+{
+    auto window = (GameWindow*)glfwGetWindowUserPointer(handle);
+    auto data = std::pair<int, bool>(key, action);
+    GameWindow::HandleEvent(window, WindowEvent::eKeyStateChanged, data);
+}
+
+void GLFW_CURSOR_POS_CALLBACK(GLFWwindow* handle, double x, double y)
+{
+    auto window = (GameWindow*)glfwGetWindowUserPointer(handle);
+    auto data = std::pair<double, double>(x, y);
+    GameWindow::HandleEvent(window, WindowEvent::eMouseMoved, data);
+}
+
+void GLFW_FRAMEBUFFER_SIZE_CALLBACK(GLFWwindow* handle, int width, int height)
+{
+    auto window = (GameWindow*)glfwGetWindowUserPointer(handle);
+    auto data = std::pair<int, int>(width, height);
+    GameWindow::HandleEvent(window, WindowEvent::eResized, data);
+}
+
+void GameWindow::HandleEvent(GameWindow* window, WindowEvent event, const std::any& data)
+{
+    for(const auto& subscriber : window->subscribers)
+    {
+        if(subscriber.event != event && subscriber.event != WindowEvent::eAny)
+            continue;
+
+        subscriber.handler(window, data);
+    }
+
+    switch(event)
+    {
+        case WindowEvent::eResized:
+        {
+            auto new_size = std::any_cast<std::pair<int, int>>(data);
+            window->width = new_size.first;
+            window->height = new_size.second;
+            break;
+        }
+    }
+}
 
 void GameWindow::create(WindowCreateInfo createInfo)
 {
+    assert(is_active == false);
 
+    int new_width = createInfo.width;
+    int new_height = createInfo.height;
+    GLFWmonitor* pMonitor = nullptr;
+
+    if(createInfo.is_fullscreen)
+    {
+        pMonitor = glfwGetPrimaryMonitor();
+
+        if(new_width == 0 && new_height == 0)
+        {
+            const GLFWvidmode* pVideoMode = glfwGetVideoMode(pMonitor);
+            new_width = pVideoMode->width;
+            new_height = pVideoMode->height;
+        }
+    }
+
+    glfwWindowHint(GLFW_RESIZABLE, createInfo.is_resizable);
+    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+    glfwWindowHint(GLFW_FOCUSED, GLFW_TRUE);
+
+    native_handle = (void*)glfwCreateWindow(new_width, new_height, createInfo.pTitle, pMonitor, nullptr);
+    if(!native_handle)
+    {
+        CDebug::Error("Could not create a new game window (glfwCreateWindow returned null).");
+        throw std::runtime_error("GameWindow-CreateError");
+    }
+
+    auto handle = reinterpret_cast<GLFWwindow*>(native_handle);
+
+    if(glfwRawMouseMotionSupported())
+    {
+        glfwSetInputMode(handle, GLFW_RAW_MOUSE_MOTION, GLFW_TRUE);
+    }
+
+    glfwSetWindowUserPointer(handle, this);
+    glfwSetKeyCallback(handle, GLFW_KEY_CALLBACK);
+    glfwSetCursorPosCallback(handle, GLFW_CURSOR_POS_CALLBACK);
+    glfwSetFramebufferSizeCallback(handle, GLFW_FRAMEBUFFER_SIZE_CALLBACK);
+
+    is_active = true;
+    width = new_width;
+    height = new_height;
+
+    GameWindow::HandleEvent(this, WindowEvent::eCreated, nullptr);
+}
+
+void GameWindow::destroy()
+{
+    assert(is_active == true);
+    glfwDestroyWindow((GLFWwindow*)native_handle);
+}
+
+bool GameWindow::should_close() const
+{
+    assert(is_active == true);
+    return glfwWindowShouldClose((GLFWwindow*)native_handle);
+}
+
+bool GameWindow::is_minimized() const
+{
+    assert(is_active == true);
+    return width == 0 && height == 0;
+}
+
+void GameWindow::update()
+{
+    assert(is_active == true);
+    glfwSwapBuffers((GLFWwindow*)native_handle);
+    glfwPollEvents();
+}
+
+void* GameWindow::handle()
+{
+    return native_handle;
+}
+
+void GameWindow::subscribe(WindowEvent event, WindowEventSubscriber handler)
+{
+    for(const auto& subscriber : subscribers)
+    {
+        auto pointer_a = subscriber.handler.target<void*>();
+        auto pointer_b = handler.target<void*>();
+
+        if(subscriber.event == event && pointer_a == pointer_b)
+        {
+            CDebug::Error("GameWindow event handler is already subscribed to the same event.");
+            throw std::runtime_error("GameWindow-EventHandler-AlreadySubscribed");
+        }
+    }
+
+    subscribers.push_back(SubscriberData
+    {
+        .event = event,
+        .handler = handler
+    });
+
+    CDebug::Log("Handler subscribed to GameWindow event.");
+}
+
+void GameWindow::unsubscribe(WindowEvent event, WindowEventSubscriber handler)
+{
+    for(auto iterator = subscribers.begin(); iterator != subscribers.end(); iterator++)
+    {
+        auto pointer_a = iterator->handler.target<void*>();
+        auto pointerB = handler.target<void*>();
+
+        if(iterator->event == event && pointer_a == pointerB)
+        {
+            iterator = subscribers.erase(iterator);
+            return;
+        }
+    }
+
+    CDebug::Error("GameWindow.unsubscribe called but handler does not exist.");
+    throw std::runtime_error("GameWindow-EventHandler-SubscriberNotFound");
 }
--- src/main.cpp
+++ src/main.cpp
@@ -3,8 +3,10 @@
 #   include <windows.h>
 #endif
 
-#include <GLFW/glfw3.h>
+#include <stdexcept>
+
 #include "utils/debug.hpp"
+#include "render/window.hpp"
 
 int main()
 {
@@ -12,6 +14,35 @@ int main()
     // Enables color formatting for the CLion terminal.
     system(("chcp " + std::to_string(CP_UTF8)).c_str());
 #   endif
+
+    try
+    {
+        GameWindow game_window;
+        game_window.create(WindowCreateInfo
+        {
+           .is_resizable = true,
+           .is_fullscreen = false,
+           .width = 1280,
+           .height = 720,
+           .pTitle = "Hello, world!"
+        });
+
+        while(!game_window.should_close())
+        {
+            if(!game_window.is_minimized())
+            {
+                // do rendering
+            }
+
+            game_window.update();
+        }
+
+        game_window.destroy();
+    } catch(std::runtime_error& error)
+    {
+        CDebug::Error("Uncaught exception: {}. Program halted.", error.what());
+        return -1;
+    }
 
     return 0;
 }
