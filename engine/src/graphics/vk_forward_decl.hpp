f5108b4cee5faa1aa13fc7b415e63b7a58482a19
ImTouchk <imtouchk@gmail.com>
2022-03-05 13:08:54 +0200

    Create convenience header with all forward declarations

---
+++ engine/src/graphics/vk_forward_decl.hpp
@@ -0,0 +1,15 @@
+#pragma once
+
+namespace Vk
+{
+    struct SurfaceWrapper;
+    struct LogicalDeviceWrapper;
+    struct MemoryAllocatorWrapper;
+    struct SwapchainWrapper;
+    struct CommandQueueWrapper;
+    struct SyncObjectsWrapper;
+
+    struct ShaderManager;
+    class BufferManager;
+    class CmdSubmitter;
+}
