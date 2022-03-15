0fb3307fbf5c2a954077a9188c54da49abc84423
ImTouchk <imtouchk@gmail.com>
2022-03-15 21:06:20 +0200

    Declare new class to vk_forward_decl

--- engine/src/graphics/vk_forward_decl.hpp
+++ engine/src/graphics/vk_forward_decl.hpp
@@ -1,4 +1,7 @@
 #pragma once
+#include "utils/identifier.hpp"
+#include <vulkan/vulkan.h>
+#include <functional>
 
 namespace Vk
 {
@@ -12,4 +15,27 @@ namespace Vk
     class RenderCallManager;
 
     constexpr unsigned MAX_FRAMES_IN_FLIGHT = 2;
+
+    struct GpuCommand
+    {
+    public:
+        GpuCommand(Identifier thread_id, VkCommandBuffer buffer);
+        GpuCommand() = default;
+
+        void destroy();
+        [[nodiscard]] VkCommandBuffer& handle();
+        [[nodiscard]] const VkCommandBuffer& handle() const;
+
+    private:
+        Identifier thread_id = 0;
+        VkCommandBuffer buffer = VK_NULL_HANDLE;
+    };
+
+    enum class GpuCommandType
+    {
+        eUnknown = 0,
+        eSecondaryMesh,
+    };
+
+    using CommandRecordFn = std::function<void(VkCommandBuffer&)>;
 }
