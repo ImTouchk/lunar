e68ac9a241a51a13e10cc16f8037e4e75f94f717
ImTouchk <imtouchk@gmail.com>
2022-03-23 11:47:57 +0200

    Merge vk_mesh and vk_object sources

--- engine/src/graphics/vk_object.cpp
+++ engine/src/graphics/vk_object.cpp
@@ -1,11 +1,40 @@
 #include "utils/identifier.hpp"
 #include "utils/debug.hpp"
+#include "render/mesh.hpp"
 #include "vk_object.hpp"
 #include "vk_shader.hpp"
 #include "vk_renderer.hpp"
 
 #include <vulkan/vulkan.h>
 #include <vk_mem_alloc.h>
+
+MeshWrapper::MeshWrapper(Vk::ObjectManager& objectManager, Identifier handle)
+	: objectManager(objectManager),
+	identifier(handle)
+{
+}
+
+void MeshWrapper::set_vertices(std::vector<Vertex>&& vertices)
+{
+	auto& data = find_by_identifier_safe(objectManager.meshes, identifier);
+	data.vertexBuffer.update(vertices.data(), vertices.size() * sizeof(Vertex));
+	data.vertexCount = vertices.size();
+	data.wasModified = true;
+}
+
+void MeshWrapper::set_indices(std::vector<Index>&& indices)
+{
+	auto& data = find_by_identifier_safe(objectManager.meshes, identifier);
+	data.indexBuffer.update(indices.data(), indices.size() * sizeof(Index));
+	data.indexCount = indices.size();
+	data.wasModified = true;
+}
+
+void MeshWrapper::use_texture(TextureWrapper&& texture)
+{
+	auto& data = find_by_identifier_safe(objectManager.meshes, identifier);
+	data.shader.use_texture(texture);
+}
 
 namespace Vk
 {
