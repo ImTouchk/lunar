7e04365c55d43b45fd34e1cfb9c581cdfdebf90d
ImTouchk <imtouchk@gmail.com>
2022-03-23 10:42:59 +0200

    Remove unused struct declaration

--- engine/src/graphics/vk_forward_decl.hpp
+++ engine/src/graphics/vk_forward_decl.hpp
@@ -16,27 +16,5 @@ namespace Vk
 
     constexpr unsigned MAX_FRAMES_IN_FLIGHT = 2;
 
-    struct GpuCommand
-    {
-    public:
-        GpuCommand(Identifier thread_id, VkCommandBuffer buffer);
-        GpuCommand() = default;
-
-        void destroy();
-        [[nodiscard]] VkCommandBuffer& handle();
-        [[nodiscard]] const VkCommandBuffer& handle() const;
-        [[nodiscard]] bool exists() const;
-
-    private:
-        Identifier thread_id = 0;
-        VkCommandBuffer buffer = VK_NULL_HANDLE;
-    };
-
-    enum class GpuCommandType
-    {
-        eUnknown = 0,
-        eSecondaryMesh,
-    };
-
     using CommandRecordFn = std::function<void(VkCommandBuffer&)>;
 }
