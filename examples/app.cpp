efb813089c81adb72c466408c92af6ee1e545a43
Mugur Vlad <imtouchk@gmail.com>
2024-08-29 00:19:34 +0300

    Template specialization for cleaner code

--- examples/app.cpp
+++ examples/app.cpp
@@ -34,10 +34,7 @@ int main(int argc, char* argv[])
     {
         Render::Window::pollEvents();
 
-        render_ctx->draw(
-            main_scene, 
-            reinterpret_cast<Render::RenderTarget*>(&game_window)
-        );
+        render_ctx->draw(main_scene, game_window);
     }
 
     //shader.destroy();
--- inc/lunar/render/render_context.hpp
+++ inc/lunar/render/render_context.hpp
@@ -2,6 +2,7 @@
 #include <lunar/api.hpp>
 #include <lunar/core/scene.hpp>
 #include <lunar/render/render_target.hpp>
+#include <concepts>
 #include <memory>
 
 namespace Render
@@ -14,6 +15,12 @@ namespace Render
 		virtual void init() = 0;
 		virtual void destroy() = 0;
 		virtual void draw(Core::Scene& scene, RenderTarget* target) = 0;
+
+		template<typename T> requires std::derived_from<T, RenderTarget>
+		inline void draw(Core::Scene& scene, T& target)
+		{
+			draw(scene, reinterpret_cast<RenderTarget*>(&target));
+		}
 	};
 
 	LUNAR_API std::shared_ptr<RenderContext> createSharedContext();
