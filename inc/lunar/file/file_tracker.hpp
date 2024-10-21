3ef13da5009c3a6f0425b726794636cc8af5676c
imtouchk <imtouchk@gmail.com>
2024-10-22 00:22:09 +0200

    Created FileTracker utility class

---
+++ inc/lunar/file/file_tracker.hpp
@@ -0,0 +1,38 @@
+#pragma once
+#include <lunar/api.hpp>
+#include <lunar/file/filesystem.hpp>
+#include <filesystem>
+#include <functional>
+#include <optional>
+#include <vector>
+
+namespace Fs
+{
+	class LUNAR_API FileTracker
+	{
+	public:
+		using EventHandler = std::function<void(Fs::Path)>;
+
+		FileTracker() = default;
+		~FileTracker() = default;
+
+		FileTracker& trackPath(const Fs::Path& path);
+		FileTracker& trackPath(const Fs::Path& path, const EventHandler& handler);
+		FileTracker& update();
+
+		bool hasChanged(const Fs::Path& path);
+
+	private:
+		struct PathData
+		{
+			bool changedFlag = false;
+			Fs::Path path;
+			size_t hash;
+			std::optional<EventHandler> handler;
+		};
+
+		size_t getFileHash(const Fs::Path& file);
+
+		std::vector<PathData> list = {};
+	};
+}
---
+++ src/file/file_tracker.cpp
@@ -0,0 +1,65 @@
+#include <lunar/file/file_tracker.hpp>
+#include <lunar/debug.hpp>
+
+namespace Fs
+{
+	FileTracker& FileTracker::trackPath(const Fs::Path& path)
+	{
+		list.push_back(PathData{
+			.path = path,
+			.hash = getFileHash(path)
+		});
+
+		return *this;
+	}
+
+	FileTracker& FileTracker::trackPath(const Fs::Path& path, const EventHandler& handler)
+	{
+		list.push_back(PathData {
+			.path    = path,
+			.hash    = getFileHash(path),
+			.handler = handler,
+		});
+
+		return *this;
+	}
+
+	FileTracker& FileTracker::update()
+	{
+		for (auto& file : list)
+		{
+			size_t old = file.hash;
+
+			file.changedFlag = false;
+			file.hash = getFileHash(file.path);
+			if (file.hash == old)
+				continue;
+
+			file.changedFlag = true;
+			if (file.handler.has_value())
+				file.handler.value()(file.path);
+		}
+
+		return *this;
+	}
+	 
+	bool FileTracker::hasChanged(const Fs::Path& path)
+	{
+		for (auto& file : list)
+		{
+			if (file.path == path)
+				return file.changedFlag;
+		}
+
+		throw;
+		return false;
+	}
+
+	size_t FileTracker::getFileHash(const Fs::Path& path)
+	{
+		using namespace std::filesystem;
+		return last_write_time(path)
+				.time_since_epoch()
+				.count();
+	}
+}
