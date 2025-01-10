10fd178cfee167da966269f119cf1210d8f6feaa
imtouchk <imtouchk@gmail.com>
2025-01-10 15:29:42 +0100

    Enable GLFW_OPENGL_FORWARD_COMPAT flag

--- src/render/window.cpp
+++ src/render/window.cpp
@@ -136,6 +136,7 @@ namespace Render
 		glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
 		glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 		glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+		glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GLFW_TRUE); // for Mac
 #		endif
 
 		handle = glfwCreateWindow(
