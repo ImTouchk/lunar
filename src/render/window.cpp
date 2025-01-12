69a437374284780c1fc7a0ac9cbc3f00ca6bee57
imtouchk <imtouchk@gmail.com>
2025-01-12 14:56:14 +0100

    Bump OpenGL version to 4.2 to use UBOs

--- src/render/window.cpp
+++ src/render/window.cpp
@@ -138,8 +138,8 @@ namespace Render
 #		endif
 
 #		ifdef LUNAR_OPENGL
-		glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-		glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+		glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+		glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
 		glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 		glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GLFW_TRUE); // for Mac
 #		endif
