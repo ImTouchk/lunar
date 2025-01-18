c5588b187bad168ac686fcc4490d6c4ed45a8689
imtouchk <imtouchk@gmail.com>
2025-01-18 23:26:29 +0100

    Classes for a basic event system

---
+++ inc/lunar/core/event.hpp
@@ -0,0 +1,37 @@
+#pragma once
+#include <lunar/api.hpp>
+#include <unordered_map>
+#include <functional>
+#include <concepts>
+#include <vector>
+
+namespace Core
+{
+	struct LUNAR_API Event {};
+
+	template<typename T = Event> requires std::derived_from<T, Event>
+	using EventListener_T = std::function<void(T&)>;
+
+	using EventListener = EventListener_T<>;
+
+	namespace imp
+	{
+		constexpr size_t MAX_LISTENERS = 10;
+
+		struct LUNAR_API EventListeners
+		{
+			EventListener callbacks[MAX_LISTENERS] = {};
+		};
+
+	}
+
+	class LUNAR_API EventHandler
+	{
+	protected:
+		void _addEventListener(size_t type, EventListener listener);
+		void _removeEventListener(size_t type, EventListener listener);
+		void _triggerEvent(size_t type, Event& e);
+			
+		std::unordered_map<size_t, imp::EventListeners> listeners;
+	};
+}
---
+++ src/core/event.cpp
@@ -0,0 +1,63 @@
+#include <lunar/core/event.hpp>
+#include <lunar/debug.hpp>
+
+namespace Core
+{
+	void EventHandler::_addEventListener(size_t type, EventListener listener)
+	{
+		auto& e_type_data = listeners[type];
+		for (size_t i = 0; i < imp::MAX_LISTENERS; i++)
+		{
+			auto& cb = e_type_data.callbacks[i];
+			if (!cb)
+			{
+				cb = listener;
+				return;
+			}
+		}
+
+		DEBUG_ASSERT(true, "Too many callbacks added to event!");
+		throw;
+	}
+
+	template<typename T, typename... U>
+	size_t getAddress(std::function<T(U...)>& f)
+	{
+		typedef T(fnType)(U...);
+		fnType** fn_ptr = f.template target<fnType*>();
+		return (size_t)*fn_ptr;
+	}
+
+	void EventHandler::_removeEventListener(size_t type, EventListener listener)
+	{
+		size_t address = getAddress(listener);
+
+		auto& e_type_data = listeners[type];
+		for (size_t i = 0; i < imp::MAX_LISTENERS; i++)
+		{
+			auto& cb = e_type_data.callbacks[i];
+			if (getAddress(cb) == address)
+			{
+				cb = nullptr;
+				return;
+			}
+		}
+
+		DEBUG_ASSERT(true, "removeEventListener called on invalid function");
+	}
+
+	void EventHandler::_triggerEvent(size_t type, Event& e)
+	{
+		auto it = listeners.find(type);
+		if (it == listeners.end())
+			return;
+
+		auto& e_type_data = it->second;
+		for (size_t i = 0; i < imp::MAX_LISTENERS; i++)
+		{
+			auto& cb = e_type_data.callbacks[i];
+			if (cb)
+				cb(e);
+		}
+	}
+}
