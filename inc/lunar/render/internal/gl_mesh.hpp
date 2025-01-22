039258173f3e5a60b8ad6fa4692a15ffbe4745a2
imtouchk <imtouchk@gmail.com>
2025-01-23 00:49:38 +0100

    Remove a good chunk of old code

--- inc/lunar/render/internal/gl_mesh.hpp
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include <glad/gl.h>
-
-namespace Render
-{
-
-}
--- inc/lunar/render/mesh.hpp
+++ inc/lunar/render/mesh.hpp
@@ -1,6 +1,5 @@
 #pragma once
 #include <lunar/render/common.hpp>
-#include <lunar/render/material.hpp>
 #include <lunar/file/filesystem.hpp>
 #include <lunar/api.hpp>
 #include <span>
@@ -8,75 +7,6 @@
 #ifdef LUNAR_OPENGL
 #	include <glad/gl.h>
 #endif
-
-//namespace Render
-//{
-//	enum class LUNAR_API MeshPrimitive
-//	{
-//		eCube  = 0,
-//		eQuad  = 1,
-//		_count = 2,
-//	};
-//
-//
-//	class LUNAR_API Mesh
-//	{
-//	public:
-//		Mesh() = default;
-//
-//		Material material = {};
-//	private:
-//#ifdef LUNAR_VULKAN
-//		VulkanBuffer      _vkIndexBuffer      = {};
-//		VulkanBuffer      _vkVertexBuffer     = {};
-//		vk::DeviceAddress _vkVertexBufferAddr = {};
-//
-//		friend class VulkanContext;
-//#endif
-//#ifdef LUNAR_OPENGL
-//		GLuint _glVao;
-//		GLuint _glVbo;
-//		GLuint _glEbo;
-//		GLuint glSsbo;
-//
-//#endif
-//		//size_t vertexCount = 0;
-//		size_t indicesCount = 0;
-//
-//		friend struct CubemapBuilder;
-//		friend struct MeshBuilder;
-//		friend class MeshRenderer;
-//		friend class RenderContext;
-//	};
-//
-//	struct LUNAR_API MeshBuilder
-//	{
-//	public:
-//		MeshBuilder() = default;
-//		~MeshBuilder() = default;
-//
-//		MeshBuilder& setVertices(const std::span<const Vertex>& vertices);
-//		MeshBuilder& setIndices(const std::span<const uint32_t>& indices);
-//		MeshBuilder& useRenderContext(std::shared_ptr<RenderContext>& context);
-//		MeshBuilder& fromGltfFile(const Fs::Path& path);
-//		MeshBuilder& build();
-//		Mesh getResult();
-//
-//	private:
-//		std::shared_ptr<RenderContext> context      = nullptr;
-//		std::vector<Vertex>            vertices     = {};
-//		std::vector<uint32_t>          indices      = {};
-//		Mesh                           result       = {};
-//		GpuMaterialData                resMaterials = {};
-//		
-//		void aggregateMesh(const fastgltf::Asset& asset, const fastgltf::Mesh& mesh);
-//		void aggregateMaterials(const fastgltf::Asset& asset);
-//
-//#		ifdef LUNAR_OPENGL
-//		void _glBuild();
-//#		endif
-//	};
-//}
 
 namespace lunar::Render
 {
